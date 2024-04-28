41a5a99c1b2ec0a8881dd177590dcec762e70a76
areal <arealgizaw@gmail.com>
2024-04-28 19:19:31 +0300

    contactus done

--- src/page/ContactUs/ContactUs.js
+++ src/page/ContactUs/ContactUs.js
@@ -2,8 +2,8 @@ import { contactUs } from "../../utils/content";
 
 const ContactUs = () => {
   return (
-    <div className=" contain ">
-      <div className=" my-1 font-bold text-[1.5em]">{contactUs.header}</div>
+    <div className=" contain text-darkGray  ">
+      <div className=" my-[5px] font-bold text-[1.5em]">{contactUs.header}</div>
       {contactUs.information.map(({ header, content, type }, i) => (
         <div key={`info${i}`}>
           <div className=" font-semibold text-[0.83em] my-[1.67em]">
